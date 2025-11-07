5bc2b797b68f570565d1c8796bce1c5b03bba773
salma riani <salmariani8@gmail.com>
2025-11-07 11:25:38 +0100

    Correction d'une ligne

--- config.php
+++ config.php
@@ -1,17 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
-        // Paramètres de connexion MySQL
-        define('DB_HOST', 'localhost'); // Hôte (souvent localhost)
-        define('DB_NAME', 'atelier_db3'); // Nom de la base de données créée
-        define('DB_USER', 'dsi23');       // Utilisateur par défaut
-        define('DB_PASS', 'dsi23admin');           // Mot de passe par défaut (souvent vide)
-    ?>
-</body>
-</html>
+
+<?php
+    // Paramètres de connexion MySQL
+    define('DB_HOST', 'localhost'); // Hôte (souvent localhost)
+    define('DB_NAME', 'atelier_db3'); // Nom de la base de données créée
+    define('DB_USER', 'dsi23');       // Utilisateur par défaut
+    define('DB_PASS', 'dsi23admin');           // Mot de passe par défaut (souvent vide)
+?>
--- connexion.php
+++ connexion.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
 require 'config.php';
 
 try {
@@ -27,5 +19,3 @@ try {
     die("Erreur de connexion : " . $e->getMessage());
 }
 ?>
-</body>
-</html>
