03309bcd43c9d2714fab9b510f7260312281bdf8
salma riani <salmariani8@gmail.com>
2025-11-07 11:23:49 +0100

    Correction d'une ligne

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
     <?php
         // Paramètres de connexion MySQL
         define('DB_HOST', 'localhost'); // Hôte (souvent localhost)
-        define('DB_NAME', 'atelier_db'); // Nom de la base de données créée
+        define('DB_NAME', 'atelier_db3'); // Nom de la base de données créée
         define('DB_USER', 'dsi23');       // Utilisateur par défaut
         define('DB_PASS', 'dsi23admin');           // Mot de passe par défaut (souvent vide)
     ?>
